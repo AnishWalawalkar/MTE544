72ea9b862ebcb902026cdd2b98661e01ee1bc5d8
timeforkevin <kevinjin@live.ca>
2017-11-03 19:16:46 -0400

    plot formatting

--- Homework-1/simulation.m
+++ Homework-1/simulation.m
@@ -40,12 +40,13 @@ u = repmat(u, 1, T/dt);
 [ x_true, y, x_est, x_cov ] = sim_motion_model(A, B, C, D, u, cov_dist, cov_meas, cov_meas, T);
 
 figure(1);
-plot(x_true(1, :), x_true(2, :));
-
+subplot(2, 2, 1)
+% X Y Plot of the True State, Measurements and Error
 hold on
+plot(x_true(1, :), x_true(2, :));
 scatter(y(1, :), y(2, :), '.');
-plot(x_est(1, :), x_est(2, :), '-.');
-for i = 1:1:150
+plot(x_est(1, :), x_est(2, :), 'g-.');
+for i = 1:3:150
     error_ellipse(x_cov(1:2, 1:2, i), x_est(1:2, i), 'style', 'm');
 end
 legend('True State', 'GPS Measurements', 'Estimated State', 'Error Ellipses');
@@ -53,12 +54,37 @@ camroll(90)
 xlabel('North [m]')
 ylabel('West [m]')
 title('EKF Implementation for Three Omni-Wheeled Robot')
+hold off
 
 
+subplot(2, 2, 2)
+time = dt:dt:15;
+plot(time, x_true(1, :), time, x_est(1, :));
+legend('True State', 'Estimated State')
+ylabel('North [m]')
+xlabel('Time [s]')
+title('EKF Estimation vs True State in X')
+
+subplot(2, 2, 3)
+time = dt:dt:15;
+plot(time, x_true(2, :), time, x_est(2, :));
+legend('True State', 'Estimated State')
+ylabel('West [m]')
+xlabel('Time [s]')
+title('EKF Estimation vs True State in Y')
+
+subplot(2, 2, 4)
+time = dt:dt:15;
+plot(time, x_true(3, :), time, x_est(3, :));
+legend('True State', 'Estimated State')
+ylabel('Yaw [rad]')
+xlabel('Time [s]')
+title('EKF Estimation vs True State in Yaw')
 %% Drive with wheel speeds Multirate
 [ x_true, y, x_est, x_cov ] = sim_motion_model(A, B, C, D, u, cov_dist, cov_meas, cov_meas_corr, T);
 
 figure(2);
+subplot(2, 2, 1)
 plot(x_true(1, :), x_true(2, :));
 
 hold on
@@ -71,7 +97,33 @@ legend('True State', 'GPS Measurements', 'Estimated State', 'Error Ellipses');
 camroll(90)
 xlabel('North [m]')
 ylabel('West [m]')
-title('Multi-Rate EKF Implementation for Three Omni-Wheeled Robot')
+title('Multi Rate EKF Implementation for Three Omni-Wheeled Robot')
+hold off
+
+
+subplot(2, 2, 2)
+time = dt:dt:15;
+plot(time, x_true(1, :), time, x_est(1, :));
+legend('True State', 'Estimated State')
+ylabel('North [m]')
+xlabel('Time [s]')
+title('EKF Estimation vs True State in X')
+
+subplot(2, 2, 3)
+time = dt:dt:15;
+plot(time, x_true(2, :), time, x_est(2, :));
+legend('True State', 'Estimated State')
+ylabel('West [m]')
+xlabel('Time [s]')
+title('EKF Estimation vs True State in Y')
+
+subplot(2, 2, 4)
+time = dt:dt:15;
+plot(time, x_true(3, :), time, x_est(3, :));
+legend('True State', 'Estimated State')
+ylabel('Yaw [rad]')
+xlabel('Time [s]')
+title('EKF Estimation vs True State in Yaw')
 
 %% drive in cirle
 
@@ -111,10 +163,6 @@ end
 
 figure(5);
 plot(x_true(1, :), x_true(2, :));
-
-
-
-
 
 
 
